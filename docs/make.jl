cba33b907d601881b2fc044ca8da906f2cf8ba13
Carlo Baldassi <carlobaldassi@gmail.com>
2016-08-23 17:43:37 +0200

    Switch to HTML docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,20 +2,21 @@ using Documenter, RRRMC
 
 makedocs(
     modules  = [RRRMC],
-    # format   = Documenter.Formats.HTML,
-    # sitename = "RRRMC.jl",
-    # pages    = Any[
-    #     "Home" => "index.md",
-    #     "Sampling algorithms" => "algorithms.md",
-    #     "Graph types" => "graph-types.md",
-    #     "Built-in graphs" => "graphs-builtin.md",
-    #     "Graphs interface" => "interface.md"
-    #    ]
+    format   = Documenter.Formats.HTML,
+    sitename = "RRRMC.jl",
+    pages    = Any[
+        "Home" => "index.md",
+        "Sampling algorithms" => "algorithms.md",
+        "Graph types" => "graph-types.md",
+        "Built-in graphs" => "graphs-builtin.md",
+        "Graphs interface" => "interface.md"
+       ]
     )
 
 deploydocs(
-    deps   = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
     repo   = "github.com/carlobaldassi/RRRMC.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing,
     julia  = "release"
 )
-
